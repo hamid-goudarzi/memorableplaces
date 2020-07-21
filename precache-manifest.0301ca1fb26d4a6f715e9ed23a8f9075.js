436ff9b95bcca83d53a03f1f5c59f38f3d711013
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2020-07-21 19:36:05 -0400

    Updates

---
+++ precache-manifest.0301ca1fb26d4a6f715e9ed23a8f9075.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f20cf70ba9a12811f3ffaf8efa0b7f23",
+    "url": "/memorableplaces/index.html"
+  },
+  {
+    "revision": "a557972c6036f47647cd",
+    "url": "/memorableplaces/static/js/2.77cbb703.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memorableplaces/static/js/2.77cbb703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cb89d904a6d7c5e1e10",
+    "url": "/memorableplaces/static/js/main.fc90f860.chunk.js"
+  },
+  {
+    "revision": "fb0ff1c92c04e53b03aa",
+    "url": "/memorableplaces/static/js/runtime-main.157fdaa4.js"
+  }
+]);
